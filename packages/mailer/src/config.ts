c1a7cc7cd655aa834f977a0c8a6103e81f72de9a
Anders Evenrud <andersevenrud@gmail.com>
2022-04-16 02:25:53 +0200

    chore(mailer): update configuration

--- packages/mailer/src/config.ts
+++ packages/mailer/src/config.ts
@@ -1,18 +1,24 @@
 const brokers = (process.env.KAFKA_BROKERS || '').split(',')
+const mailerHost = process.env.MAILER_HOST || 'mailhog'
+const mailerPort = parseInt(process.env.MAILER_PORT || '1025')
 
 export default {
   appUrl: process.env.APP_URL || 'http://localhost:8080/',
   from: 'no-reply@mailer.app',
   waitOn: {
-    resources: [...brokers.map((s) => `tcp:${s}`)],
+    resources: [
+      ...brokers.map((s) => `tcp:${s}`),
+      // NOTE: This is purely for development purposes.
+      `tcp:${mailerHost}:${mailerPort}`,
+    ],
   },
   kafka: {
     clientId: 'mailer',
     brokers,
   },
   mailer: {
-    host: process.env.MAILER_HOST || 'mailhog',
-    port: parseInt(process.env.MAILER_PORT || '1025'),
+    host: mailerHost,
+    port: mailerPort,
     secure: false,
   },
 }
