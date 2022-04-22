4212725c36dab3a9bdeb96c31cddfa97bfba7c45
Anders Evenrud <andersevenrud@gmail.com>
2022-04-22 22:43:07 +0200

    feat(cli): add topic updates in migration script

--- packages/cli/scripts/topics.ts
+++ packages/cli/scripts/topics.ts
@@ -1,6 +1,49 @@
 import waitOn from 'wait-on'
-import { Kafka } from 'kafkajs'
+import { Kafka, ConfigResourceTypes } from 'kafkajs'
 import config from '../src/config'
+
+const topics = [
+  {
+    topic: 'clientState',
+    configEntries: [
+      {
+        name: 'retention.ms',
+        value: '604800000',
+      },
+    ],
+  },
+  {
+    topic: 'clientAction',
+    configEntries: [
+      {
+        name: 'retention.ms',
+        value: '604800000',
+      },
+    ],
+  },
+  {
+    topic: 'clientMessage',
+    configEntries: [
+      {
+        name: 'retention.ms',
+        value: '604800000',
+      },
+    ],
+  },
+  {
+    topic: 'logs',
+    configEntries: [],
+  },
+  {
+    topic: 'mailNotification',
+    configEntries: [
+      {
+        name: 'retention.ms',
+        value: '604800000',
+      },
+    ],
+  },
+]
 
 async function main() {
   try {
@@ -15,48 +58,34 @@ async function main() {
 
     await admin.connect()
 
+    const found = await admin.fetchTopicMetadata()
+
+    const updates = []
+    const creations = []
+
+    for (const topic of topics) {
+      const foundTopic = found.topics.find((t) => t.name === topic.topic)
+      if (foundTopic) {
+        updates.push(topic)
+      } else {
+        creations.push(topic)
+      }
+    }
+
+    console.info('Creating', creations.length, 'topics')
+    console.info('Updating', creations.length, 'topics')
+
     await admin.createTopics({
-      topics: [
-        {
-          topic: 'clientState',
-          configEntries: [
-            {
-              name: 'retention.ms',
-              value: '604800000',
-            },
-          ],
-        },
-        {
-          topic: 'clientAction',
-          configEntries: [
-            {
-              name: 'retention.ms',
-              value: '604800000',
-            },
-          ],
-        },
-        {
-          topic: 'clientMessage',
-          configEntries: [
-            {
-              name: 'retention.ms',
-              value: '604800000',
-            },
-          ],
-        },
-        {
-          topic: 'logs',
-        },
-        {
-          topic: 'mailNotification',
-          configEntries: [
-            {
-              name: 'retention.ms',
-              value: '604800000',
-            },
-          ],
-        },
-      ],
+      topics: creations,
+    })
+
+    await admin.alterConfigs({
+      validateOnly: false,
+      resources: updates.map(({ topic, configEntries }) => ({
+        type: ConfigResourceTypes.TOPIC,
+        name: topic,
+        configEntries,
+      })),
     })
 
     await admin.disconnect()
