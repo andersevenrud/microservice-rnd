47e05051228e6ee3eb1616e0d4da401967d20db2
Anders Evenrud <andersevenrud@gmail.com>
2022-04-24 03:02:05 +0200

    docs(cli): explain cron script usage

--- packages/cli/src/cron.ts
+++ packages/cli/src/cron.ts
@@ -1,6 +1,12 @@
 import { spawn } from 'child_process'
 import { schedule } from 'node-cron'
 
+/*
+ * This is only used in the docker-compose development environment.
+ * On a kubernetes cluster this is not used. Instead a cron job is applied
+ * to the kubernetes pod.
+ */
+
 schedule('0 * * * *', () => {
   spawn('npm', ['run', 'cleanup'], {
     stdio: 'inherit',
