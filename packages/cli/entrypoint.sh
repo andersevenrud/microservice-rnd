0ac694665e4e6a70b38de87d595686faab7e6792
Anders Evenrud <andersevenrud@gmail.com>
2022-04-28 01:28:28 +0200

    docs(docker): add development notations to entrypoints

--- packages/cli/entrypoint.sh
+++ packages/cli/entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+#
+# This is only used for development enviroments
+# Like the docker-compose.yml setup
+#
 
 if [ -z "$1" ]; then
   npm ci --silent --no-audit --no-fund
--- packages/mailer/entrypoint.sh
+++ packages/mailer/entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+#
+# This is only used for development enviroments
+# Like the docker-compose.yml setup
+#
 
 npm ci --silent --no-audit --no-fund
 exec npm run dev
