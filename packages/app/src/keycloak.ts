58d3cfaa1eebd088f7d7a6b20a3d4907b6fa2b64
Anders Evenrud <andersevenrud@gmail.com>
2022-05-04 02:31:29 +0200

    feat(app): add api access tokens

--- packages/app/src/keycloak.ts
+++ packages/app/src/keycloak.ts
@@ -1,9 +1,9 @@
 import Keycloak from 'keycloak-js'
 
 const keycloak = new Keycloak({
-  url: import.meta.env.KEYCLOAK_URL || 'http://localhost:8081/',
-  realm: import.meta.env.KEYCLOAK_REALM || 'rnd',
-  clientId: import.meta.env.KEYCLOAK_CLIENT_ID || 'rnd-client',
+  url: import.meta.env.VITE_KEYCLOAK_URL || 'https://auth.rnd.lvh.me/',
+  realm: import.meta.env.VITE_KEYCLOAK_REALM || 'rnd',
+  clientId: import.meta.env.VITE_KEYCLOAK_CLIENT_ID || 'rnd-client',
 })
 
 export default keycloak
