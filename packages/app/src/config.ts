3fc2450cdc5bfc0af9cca3a313a0535c1459f0d1
Anders Evenrud <andersevenrud@gmail.com>
2022-05-08 19:37:57 +0200

    chore(app): better defaults for keycloak connection

--- packages/app/src/config.ts
+++ packages/app/src/config.ts
@@ -4,7 +4,9 @@ export default {
   webSocketUrl: window.location.origin.replace(/^http/, 'ws') + '/api/logs/',
 
   keycloak: {
-    url: import.meta.env.VITE_KEYCLOAK_URL || 'https://auth.rnd.lvh.me/',
+    url:
+      import.meta.env.VITE_KEYCLOAK_URL ||
+      `https://auth.${window.location.hostname}/`,
     realm: import.meta.env.VITE_KEYCLOAK_REALM || 'rnd',
     clientId: import.meta.env.VITE_KEYCLOAK_CLIENT_ID || 'rnd-client',
   },
